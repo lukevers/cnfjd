c57e4cd723b1a817c12042ae672473aeea00e4c0
Luke Evers <me@lukevers.com>
2014-12-31 12:31:11 -0500

    Rename a lot of procs, and generate configuration files

--- cnfjd.nim
+++ cnfjd.nim
@@ -1,13 +1,21 @@
 from os import existsFile, existsDir, removeDir, getCurrentDir, setCurrentDir, execShellCmd, copyFileWithPermissions
 
 ##
-## checkInstalled
+## isInstalled
 ##
 ## This proc checks to see if we already have cjdns installed on
 ## our system.
 ##
-proc checkInstalled(): bool =
+proc isInstalled(): bool =
   return os.existsFile("/usr/local/bin/cjdroute")
+
+##
+## hasConfig
+##
+## This proc checks to see if we have a configuration file already
+## on our system or not.
+proc hasConfig(): bool =
+  return os.existsFile("~/.cjdroute.conf")
 
 ##
 ## yes
@@ -22,6 +30,14 @@ proc yes(question: string): bool =
     case readLine(stdin)
     of "y", "Y", "yes", "Yes": return true
     else: return false
+
+##
+## generateConfig
+##
+## This proc generates a new configuration file.
+##
+proc generateConfig(): bool {.discardable.} =
+  return os.execShellCmd("cjdroute --genconf > ~/.cjdroute.conf") == 0
 
 ##
 ## installCjdns
@@ -59,21 +75,21 @@ proc installCjdns(): bool {.discardable.} =
   return true
 
 ##
-## checkIfUpTodate
+## isUpToDate
 ##
 ## TODO
 ##
 ## This proc checks to see if cjdns is up to date or not.
 ##
-proc checkIfUpTodate(): bool =
-  # For now let's just return true until we do it.
-  return true
+proc isUpToDate(): bool =
+  # For now let's just return false until we do it.
+  return false
 
 # First let's check to see if it's installed or not. If it's
 # already installed then we don't have to do some other
 # stuff, and we can make it quicker for people to use
 # this for other things besides installation.
-let installed = checkInstalled()
+let installed = isInstalled()
 
 # Now if we don't have cjdns installed already, that means
 # we have to install cjdns. Let's install cjdns!
@@ -82,9 +98,17 @@ if not installed:
   if yes("It doesn't seem like cjdns is installed. Would you like to install cjdns?"):
     installCjdns()
 
+# Now we check to see if we have a configuration file yet.
+# If we don't, then lets ask if we want to generate one.
+if installed and not hasConfig():
+  # First let's ask to make sure they want to generate one.
+  if yes("It looks like you don't have a configuration file. Would you like to generate one?"):
+    generateConfig()
+  else: echo "If you already have one, place it at ~/.cjdroute.conf"
+
 # If we have gotten this far then cjdns is already installed.
 # Now it's time to check if cjdns is up to date!
-if installed and not checkIfUpTodate():
+if installed and not isUpToDate():
   # First let's ask to make sure we want to update cjdns.
   if yes("It looks like there's a newer version of cjdns available! Would you like to update cjdns?"):
     installCjdns()
